65a34d72a89ede364f0f4acdfe9bb5173a80dd9c
CodeResin <coderesin@gmail.com>
2025-12-05 09:28:22 +0800

    rollup,eslint,pret

--- packages/mini-markdown-ast-parser/rollup.config.ts
+++ packages/mini-markdown-ast-parser/rollup.config.ts
@@ -1,44 +1,50 @@
-import { defineConfig } from 'rollup'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import alias from '@rollup/plugin-alias'
-import typescript from '@rollup/plugin-typescript'
-import del from 'rollup-plugin-delete'
-import rosolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import postcss from 'rollup-plugin-postcss'
+import { defineConfig } from "rollup";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import alias from "@rollup/plugin-alias";
+import typescript from "@rollup/plugin-typescript";
+import del from "rollup-plugin-delete";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import postcss from "rollup-plugin-postcss";
+// import { babel } from "@rollup/plugin-babel";
+// import { terser } from "rollup-plugin-terser";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: [
     {
-      file: 'dist/index.cjs',
-      format: 'cjs'
+      file: "dist/index.cjs",
+      format: "cjs",
     },
     {
-      file: 'dist/index.js',
-      format: 'esm'
-    }
+      file: "dist/index.js",
+      format: "esm",
+    },
   ],
-  external: ['highlight.js'],
+  external: ["highlight.js"],
   plugins: [
     postcss({
-      extract: 'style/index.css',
-      minimize: true
+      extract: "style/index.css",
+      minimize: true,
     }),
-    rosolve(),
+    resolve(),
+    // 没有这个插件，Rollup 找不到 lodash
+    // import _ from 'lodash'  // ❌ 错误：无法解析 lodash
     commonjs(),
-    del({ targets: ['dist'] }),
+    del({ targets: ["dist"] }),
+    // 在每次打包前删除 dist目录
+    // 避免旧文件残留导致问题
     alias({
-      entries: [{ find: '@', replacement: path.resolve(__dirname, './src') }]
+      entries: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
     }),
     typescript({
-      tsconfig: path.resolve(__dirname, './tsconfig.json'),
-      declaration: true,
-      declarationDir: path.resolve(__dirname, './dist/types')
-    })
-  ]
-})
+      tsconfig: path.resolve(__dirname, "./tsconfig.json"),
+      declaration: true, // ⭐ 生成 .d.ts 类型声明文件
+      declarationDir: path.resolve(__dirname, "./dist/types"), // 声明文件输出目录
+    }),
+  ],
+});
--- packages/mini-markdown-ast-parser/src/types/tokens-types.ts
+++ packages/mini-markdown-ast-parser/src/types/tokens-types.ts
@@ -1,8 +1,9 @@
-import type { TOKENS_TYPES } from '../common/tokens-types'
+import type { TOKENS_TYPES } from "../common/tokens-types";
 
 // 导出分词类型（也就是区分当前是什么类型的节点）
-export type TokensTypes = Record<keyof typeof TOKENS_TYPES, TokenTypeVal>
+export type TokensTypes = Record<keyof typeof TOKENS_TYPES, TokenTypeVal>;
 // 分词类型的 key
-export type TokensTypesKey = keyof TokensTypes
+export type TokensTypesKey = keyof TokensTypes;
+export type TokensTypesKey1 = keyof typeof TOKENS_TYPES;
 // 分词类型的 value
-export type TokenTypeVal = typeof TOKENS_TYPES[keyof typeof TOKENS_TYPES]
+export type TokenTypeVal = (typeof TOKENS_TYPES)[keyof typeof TOKENS_TYPES];
